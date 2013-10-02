615ab209f48292af48dbcc3aeace142c483e7ccc
saeedalavinia <saeed.alavinia@yahoo.ca>
2013-10-02 16:28:58 -0400

    comments improved

--- server/src/server/TcpServer.java
+++ server/src/server/TcpServer.java
@@ -4,7 +4,6 @@ import java.io.*;
 import java.net.*;
 
 public class TcpServer {
-	String clientSentence;
 	ServerSocket Socket;
 	Socket connectionSocket;
 	Thread invokerThread, flasherThread;
@@ -12,6 +11,7 @@ public class TcpServer {
 
 	public void serverSetup(){
 		
+			// wait till the invoker device is connected and fire up its realted thread
 		
 			try {
 				Socket = new ServerSocket(5006);
@@ -20,20 +20,24 @@ public class TcpServer {
 				System.err.println("connection gone wrong!");
 				e.printStackTrace();
 			}
+			//instantiate and start invoker thread
 			Thread invokerThread= new Thread(new InvokerThread(connectionSocket));
 			invokerThread.start();
 			
 			
+			// wait till the flasher device is connected and fire up its realted thread
 			try {
 				Socket = new ServerSocket(5006);
 				connectionSocket = Socket.accept();
 			} catch (IOException e) {
 				System.err.println("connection gone wrong!");
 				e.printStackTrace();
-			}		
+			}	
+			//instantiate and start flasher thread
 			Thread FlasherThread= new Thread(new FlasherThread(connectionSocket));
 			FlasherThread.start();
 		
+			// wait till all threads are done with their execution before destroying the main thread
 			try {
 				invokerThread.join();
 				FlasherThread.join();
@@ -46,6 +50,8 @@ public class TcpServer {
 	
 
 	public static void main(String[] args) {
+		
+		//Initialize the system
 		TcpServer server= new TcpServer();
 		server.serverSetup();
 			
